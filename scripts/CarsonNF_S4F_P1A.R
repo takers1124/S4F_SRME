fe9ebcdda4ab17b07856ba32dc8e80bc8ee39222
TaylorAkers <taylor.akers@usda.gov>
2026-03-06 14:07:16 -0700

    completed steps 4 and 5

--- scripts/CarsonNF_S4F_P1A.R
+++ scripts/CarsonNF_S4F_P1A.R
@@ -327,114 +327,48 @@ plot(is.na(CNF_combined_rast))
 ## stats ----
 # we want to know what % of the CNF each priority factor (PF) & combo occupies
 # need a total # cells in the CNF to compare
-global(CNF_DEM_rast, fun = "notNA") # 9199894 cells (covers all CNF)
+global(CNF_DEM_rast, fun = "notNA") # 8926021 cells (covers all CNF)
 # but not same resolution as rest of data
 DEM_resampled <- resample(CNF_DEM_rast, CNF_EVH_filt_rast, method = "bilinear")
 # now has same "standard" resolution and extent (see above)
-global(DEM_resampled, fun = "notNA") # 7773990 cells (covers all CNF)
+global(DEM_resampled, fun = "notNA") # 7180630 cells (covers all CNF)
 
 
 ### independent PFs  ----
 #### QMD ----
 # all areas with QMD values
-global(CNF_QMD_rast, fun = "notNA") # 5697616 cells
-(5697616/7773990)*100 # 73.29076 % of CNF has QMD values
+global(CNF_QMD_rast, fun = "notNA") # 5641440 cells
+(5641440/7180630)*100 # 78.56469 % of CNF has QMD values
 
 # areas with QMD > 5 inches
-global(CNF_QMD_filt_rast, fun = "notNA") # 4160703
-(4160703/7773990)*100 # 53.52082 % of CNF has trees > 5 in QMD
+global(CNF_QMD_filt_rast, fun = "notNA") # 4420737
+(4420737/7180630)*100 # 61.56475 % of CNF has trees > 5 in QMD
 
 #### EVH ----
-# all veg area
-global(EVH_CNF >= 101, fun = "sum", na.rm = TRUE) # 7001131 cells
-(7001131/7773990)*100 # 90.0584 % of CNF is vegetated 
-
 # all tree area
-global(CNF_EVH_rast, fun = "notNA") # 5311714
-(5311714/7773990)*100 # 68.32674 % of CNF has trees 
+global(CNF_EVH_rast, fun = "notNA") # 5618557
+(5618557/7180630)*100 # 78.24602 % of CNF has trees 
 
 # trees > 10 ft area
-global(CNF_EVH_filt_rast, fun = "notNA") # 5231674
-(5231674/7773990)*100 # 67.29715 % of CNF has trees > 10 ft
+global(CNF_EVH_filt_rast, fun = "notNA") # 5024646
+(5024646/7180630)*100 # 69.975 % of CNF has trees > 10 ft
 
 #### slope ----
 # need to use resampled version (above) to get same resolution and extent
-global(slope_resampled, fun = "notNA") # 6282487 cells 
-(6282487/7773990)*100 # 80.81419 % remaining after 24* filter
+global(slope_resampled, fun = "notNA") # 6378993 cells 
+(6378993/7180630)*100 # 88.83612 % remaining after 24* filter
 
 #### road ----
-global(CNF_road_filt_rast, fun = "notNA") # 5316596 cells
-# entire CNF = 7773990 cells
-(5316596/7773990)*100 # 68.38954 % remaining
+global(CNF_road_filt_rast, fun = "notNA") # 5675161 cells
+(5675161/7180630)*100 # 79.0343 % remaining
 
 
 ### combined PFs ----
-# we want to know what % of the CNF each category falls into after combining
+# we want to know what % of the CNF meets each priority factor's threshold, after combining
 
-# value 5, QMD only
-global(CNF_combined_rast == 5, fun = "sum", na.rm = TRUE) # 24212 cells
-(24212/7773990)*100 # 0.3114488 % of CNF
-
-# value 10, EVH only
-global(CNF_combined_rast == 10, fun = "sum", na.rm = TRUE) # 86352 cells
-(86352/7773990)*100 # 1.110781 % of CNF
-
-# value 15, QMD + EVH
-global(CNF_combined_rast == 15, fun = "sum", na.rm = TRUE) # 176429 cells
-(176429/7773990)*100 # 2.269478 % of CNF
-
-# value 100, slope only
-global(CNF_combined_rast == 100, fun = "sum", na.rm = TRUE) # 589281 cells
-(589281/7773990)*100 # 7.580162 % of CNF
-
-# value 105, slope + QMD
-global(CNF_combined_rast == 105, fun = "sum", na.rm = TRUE) # 84525 cells
-(84525/7773990)*100 # 1.08728 % of CNF
-
-# value 110, slope + EVH
-global(CNF_combined_rast == 110, fun = "sum", na.rm = TRUE) # 391693 cells
-(391693/7773990)*100 # 5.038507 % of CNF
-
-# value 115, slope + EVH + QMD
-global(CNF_combined_rast == 115, fun = "sum", na.rm = TRUE) # 790573 cells
-(790573/7773990)*100 # 10.16946 % of CNF
-
-# value 500, road only
-global(CNF_combined_rast == 500, fun = "sum", na.rm = TRUE) # 215441 cells
-(215441/7773990)*100 # 2.771305 % of CNF
-
-# value 505, road + QMD
-global(CNF_combined_rast == 505, fun = "sum", na.rm = TRUE) # 52812 cells
-(52812/7773990)*100 # 0.6793423 % of CNF
-
-# value 510, road + EVH
-global(CNF_combined_rast == 510, fun = "sum", na.rm = TRUE) # 169563 cells
-(169563/7773990)*100 # 2.181158 % of CNF
-
-# value 515, road + EVH + QMD
-global(CNF_combined_rast == 515, fun = "sum", na.rm = TRUE) # 452365 cells
-(452365/7773990)*100 # 5.818955 % of CNF
-
-# value 600, road + slope
-global(CNF_combined_rast == 600, fun = "sum", na.rm = TRUE) # 1002984 cells
-(1002984/7773990)*100 # 12.90179 % of CNF
-
-# value 605, road + slope + QMD
-global(CNF_combined_rast == 605, fun = "sum", na.rm = TRUE) # 258732 cells
-(258732/7773990)*100 # 3.328175 % of CNF
-
-# value 610, road + slope + EVH
-global(CNF_combined_rast == 610, fun = "sum", na.rm = TRUE) # 843644 cells
-(843644/7773990)*100 # 10.85214 % of CNF
-
-# value 615, road + slope + QMD + EVH
-global(CNF_combined_rast == 615, fun = "sum", na.rm = TRUE) # 2321055 cells
-(2321055/7773990)*100 # 29.85668 % of CNF
-
-# value notNA
-global(CNF_combined_rast, fun = "notNA") # 7459661 cells
-(7459661/7773990)*100 # 95.95666 % of CNF (equals the sum of above %s)
-100-95.95666 # 4.04334 % is NA (QMD < 5in, EVH < 10ft, slope >24, road >0.57)
+# value 615 = road + slope + QMD + EVH
+global(CNF_combined_rast == 615, fun = "sum", na.rm = TRUE) # 2734478 cells
+(2734478/7180630)*100 # 38.08131 % of CNF
 
 
 ## filter & adjust value ----
@@ -444,17 +378,19 @@ CNF_priority_rast <- ifel(
   1, NA)
 
 # just confirm filter
-global(CNF_priority_rast, fun = "notNA") # 2321055 cells (same as value=615 above)
-(2321055/7773990)*100 # 29.85668 % of CNF
+global(CNF_priority_rast, fun = "notNA") # 2734478 cells (same as value=615 above)
+(2734478/7180630)*100 # 38.08131 % of CNF
 
 
 ## calc area ---- 
 # transform = FALSE bc already an equal-area projection, EPSG: 5070, Conus Albers
 # default units are m^2
-expanse(CNF_priority_rast, transform = FALSE) # 2088949500 m^2
-2088949500/4046.86 # 4046.86 m2/acre = 516190.2 acres
-# entire CNF = 1723619 acres (calculated from CNF_vect polygon in Part1A_2)
-(516190.2/1723619)*100 # 29.94805 % of CNF (almost same as value=615 above)
+expanse(CNF_priority_rast, transform = FALSE) # 2461030200 m^2
+2461030200/4046.86 # 4046.86 m2/acre = 608133.3 acres
+
+# entire CNF = 1592473 acres (calculated from CNF_vect polygon in Part1A_2)
+(608133.3/1592473)*100 # 38.18798 % of CNF (almost same as value=615 above)
+
 
 ## viz ----
 plot(CNF_priority_rast, col = "goldenrod1")
@@ -470,12 +406,12 @@ CNF_priority_rast <- rast("CNF_priority_rast.tif")
 ## patches ----
 # btw this line took ~20 minutes to run
 priority_patches_all <- patches(CNF_priority_rast, directions=4, values=FALSE, zeroAsNA=FALSE, allowGaps=FALSE)
-# there are 95527 patches
+# there are 91498 patches
 
 
 ## make polygons ----
 patch_all_polys <- as.polygons(priority_patches_all, values = FALSE)
-# there are 95527 geometries 
+# there are 91498 geometries 
 
 # add a patch_ID attribute for each poly
 patch_all_polys$patch_ID <- 1:nrow(patch_all_polys) 
@@ -487,20 +423,20 @@ patch_all_polys$patch_acres <- expanse(patch_all_polys) * 0.000247105
 
 # filter out small poys (< 20 acres)
 small_polys_removed <- patch_all_polys[patch_all_polys$patch_acres >= 20, ]
-# 1429 geoms remain
-(1429/134187)*100 # 1.064932 % of polys remain (are >= 20 acres)
-# so ~99 % of patches/polys were < 20 acres (isolated areas)
+# 1073 geoms remain
+(1073/134187)*100 # 0.7996304 % of polys remain (are >= 20 acres)
+# so ~99.2 % of patches/polys were < 20 acres (isolated areas)
 # but many of these remaining polys are quite large and need to be divided
 
 # separate mid-sized polys (20-200 acres)
 mid_polys <- small_polys_removed[small_polys_removed$patch_acres <= 200, ]
-# 1196 geoms
-(1196/1414)*100 # 84.58274 % of polys >= 20 acres are also <= 200 acres
+# 917 geoms
+(917/1073)*100 # 85.46132 % of polys >= 20 acres are also <= 200 acres
 # these don't need to be divided
 
 # separate large polys ( > 200 acres)
 large_polys <- small_polys_removed[small_polys_removed$patch_acres > 200, ]
-# 233 geoms
+# 156 geoms
 # these do need to be divided
 
 
@@ -524,11 +460,11 @@ divided_polys_list <- lapply(1:nrow(large_polys), function(i) {
 
 # combine all divided polys into a single SpatVector
 divided_polys_vect <- do.call(rbind, divided_polys_list)
-# 2910 geoms
+# 3835 geoms
 
 # combine the mid-sized polys with the newly divided large polys
 CNF_PCUs_1A_vect <- rbind(mid_polys, divided_polys_vect)
-# 4106 geoms
+# 4752 geoms
 
 
 ## adjust ----
@@ -537,7 +473,7 @@ CNF_PCUs_1A_vect$PCU_ID <- 1:nrow(CNF_PCUs_1A_vect)
 CNF_PCUs_1A_vect$area_acres <- expanse(CNF_PCUs_1A_vect) * 0.000247105
 
 summary(CNF_PCUs_1A_vect)
-# area_acres min = 20.02, max = 265.60  
+# area_acres min = 17.62, max = 342.38    
 # not exactly within the desired 20-200 acre range, but close enough
 # this is a step in the method that we could refine in the future
 
@@ -546,20 +482,14 @@ CNF_PCUs_1A_vect <- CNF_PCUs_1A_vect[, c("PCU_ID", "area_acres")]
 
 CNF_PCUs_1A_df <- as.data.frame(CNF_PCUs_1A_vect)
 
-sum(CNF_PCUs_1A_vect$area_acres) # 422214.1 acres
-sum(small_polys_removed$patch_acres) # 422214.1 acres
+sum(CNF_PCUs_1A_vect$area_acres) # 530520.2 acres
+sum(small_polys_removed$patch_acres) # 530520.2 acres
 # bc these are =, we know the divide function worked (retained all area)
 
 
 ## stats ----
-# CNF is 1723619 acres 
-(422214.1/1723619)*100 # 24.49579 % of CNF are highest priority areas (PCUs)
-
-# for reference, 
-(506182.4/1723619)*100 # 29.36742 % of CNF meets PFs (CNF_priority_rast values = 1)
-
-(422214.1/506182.4)*100 # 83.41145 % of the areas that meet basic priorities
-# are continuous PCUs > 20 acres
+# CNF is 1592473 acres 
+(422214.1/1592473)*100 # 26.51311 % of CNF are highest priority areas (PCUs)
 
 
 ## viz ----
