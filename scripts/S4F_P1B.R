3de31ce982cf14908f147970a1a78951f4762238
TaylorAkers <taylor.akers@usda.gov>
2026-03-06 14:07:33 -0700

    started new script for part1_B

---
+++ scripts/S4F_P1B.R
@@ -0,0 +1,27 @@
+# Seeds 4 the Future - Prioritizing cone collection for future-focused reforestation 
+
+# Part 1B: adding attributes to Potential Collection Units (PCUs)
+
+# (1) setup ----
+
+library(terra) 
+library(tidyterra) 
+library(dplyr)
+
+
+## read PCU vector ----
+
+
+# (2) Pre-process data ----
+## (A) crown fire probability ----
+
+
+## (B) nursery inventory ----
+
+
+## (C) 
+
+
+
+
+# (3) Extract values ----
